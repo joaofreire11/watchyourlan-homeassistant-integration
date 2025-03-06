8a6d03562a1bb3f6de0281ab0568ae3e5a9b416d
joao.freire <joao.freire@innowave.tech>
2025-03-06 15:45:15 +0000

    Fixing setup code

--- custom_components/watchyourlan/__init__.py
+++ custom_components/watchyourlan/__init__.py
@@ -22,7 +22,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     await coordinator.async_config_entry_first_refresh()
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
 
-    hass.config_entries.async_setup_platforms(entry, ["sensor"])
+    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
 
     return True
 
